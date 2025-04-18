98b6b68474f5a09c0fbe428ff8f1b122656dedf6
Sajjad C Kareem <sajjadck04@gmail.com>
2025-04-18 12:28:53 -0400

    feat: implemented rhead with support of -n flag

--- rhead/src/main.rs
+++ rhead/src/main.rs
@@ -1,3 +1,53 @@
+use clap::Parser;
+use std::fs::read_to_string;
+use std::io::{self, BufRead};
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "rhead",
+    about = "A simple UNIX head command",
+    long_about = "This filter displays the first count lines or bytes of each of the specified files, 
+    or of the standard input if no files are specified.  If count is omitted it defaults to 10.",
+    help_template = "{about}\n\nUsage: {name} [OPTIONS] [file ...]\n\n{all-args}"
+)]
+
+struct Cli {
+    #[arg(
+        short = 'n',
+        help = "Displays the first count lines of each specified file.",
+        default_value_t = 10
+    )]
+    count: usize,
+    files: Vec<String>,
+}
+
+fn read_stdin(cli: &Cli) {
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+
+    for line in handle.lines().take(cli.count) {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(e) => eprintln!("Error reading line: {}", e),
+        }
+    }
+}
+
+fn read_file(file: &str, cli: &Cli) {
+    read_to_string(file)
+        .unwrap()
+        .lines()
+        .take(cli.count)
+        .for_each(|line| println!("{}", line));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if cli.files.is_empty() {
+        read_stdin(&cli);
+    }
+    for file in &cli.files {
+        read_file(&file, &cli);
+    }
 }
