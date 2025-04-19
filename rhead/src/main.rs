a82d789094671852a035b126c1a1eb8ca0d1c044
Sajjad C Kareem <sajjadck04@gmail.com>
2025-04-18 23:51:25 -0400

    doc: updated rhead help documentation

--- rhead/src/main.rs
+++ rhead/src/main.rs
@@ -8,7 +8,8 @@ use std::io::{self, BufRead};
     about = "A simple UNIX head command",
     long_about = "This filter displays the first count lines or bytes of each of the specified files, 
     or of the standard input if no files are specified.  If count is omitted it defaults to 10.",
-    help_template = "{about}\n\nUsage: {name} [OPTIONS] [file ...]\n\n{all-args}"
+    help_template = "{about}\n\n{usage-heading}\n    {usage}\n\n{all-args}{after-help}",
+    after_help = "\nEXAMPLES:\n    rhead file.txt         # Show first 10 lines of file.txt\n    rhead -n 5 file.txt    # Show first 5 lines of file.txt\n    cat file.txt | rhead   # Show first 10 lines from stdin"
 )]
 
 struct Cli {
