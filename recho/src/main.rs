d1535a3c2da38f1bc5a821e66e3c49b5c69612f0
Sajjad C Kareem <sajjadck04@gmail.com>
2025-04-18 23:51:19 -0400

    doc: updated recho help documentation

--- recho/src/main.rs
+++ recho/src/main.rs
@@ -4,7 +4,9 @@ use clap::Parser;
 #[command(
     name = "recho",
     about = "A simple UNIX echo command",
-    help_template = "{about}\n\nUsage: {name} [OPTIONS] <STRING>\n\n{all-args}"
+    long_about = "The recho utility writes any specified operands, separated by single blank (' ') characters and followed by a newline ('\\n') character, to the standard output.",
+    help_template = "{about}\n\n{usage-heading}\n    {usage}\n\n{all-args}{after-help}",
+    after_help = "\nEXAMPLES:\n    recho hello world     # Print 'hello world' with newline\n    recho -n hello       # Print 'hello' without newline"
 )]
 struct Cli {
     #[arg(short = 'n', help = "Do not output the trailing newline")]
